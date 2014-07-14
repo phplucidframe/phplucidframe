2034465e1ab8d46deed475da6b6237ad88c92604
Sithu <cithukyaw@gmail.com>
2014-07-14 12:52:21 +0630

    Added more prominent functions for session and flash setters/getters.

--- helpers/session_helper.php
+++ helpers/session_helper.php
@@ -19,10 +19,17 @@
  *
  * @param $name string The session variable name to store the value
  *		It can be a value separated by period, eg., user.name will be ['user']['name']
- * @param $value mixed The value to be stored. 
- * @param $serialize boolean The value is to be serialized or not
+ * @param mixed 	$value The value to be stored. 
+ * @param boolean 	$serialize The value is to be serialized or not
  *
  * @return void
+ */
+function session_set($name, $value='', $serialize=false){
+	setSession($name, $value, $serialize);
+}
+/**
+ * Alias of session_set()
+ * Set a message or value in Session using a name
  */
 function setSession($name, $value='', $serialize=false){
 	if(strpos($name, '.') !== false){
@@ -41,11 +48,18 @@ function setSession($name, $value='', $serialize=false){
 /**
  * Get a message or value of the given name from Session
  * 
- * @param $name string The session variable name to retrieve its value
- *			It can be a value separated by period, eg., user.name will be ['user']['name']
- * @param $unserialize boolean The value is to be unserialized or not
+ * @param string $name 	The session variable name to retrieve its value
+ 						It can be a value separated by period, eg., user.name will be ['user']['name']
+ * @param boolean $unserialize The value is to be unserialized or not
  *
  * @return mixed The value from SESSION
+ */
+function session_get($name, $unserialize=false){
+	return getSession($name, $unserialize);
+}
+/**
+ * Alias of session_get()
+ * Get a message or value of the given name from Session
  */
 function getSession($name, $unserialize=false){
 	if(strpos($name, '.') !== false){
@@ -64,8 +78,15 @@ function getSession($name, $unserialize=false){
 /**
  * Delete a message or value of the given name from Session
  * 
- * @param $name string The session variable name to delete its value
+ * @param string $name The session variable name to delete its value
  * @return void
+ */
+function session_delete($name){
+	deleteSession($name);
+}
+/**
+ * Alias of session_delete()
+ * Delete a message or value of the given name from Session
  */
 function deleteSession($name){
 	if(strpos($name, '.') !== false){
@@ -79,16 +100,27 @@ function deleteSession($name){
 	}	
 }
 
-if(!function_exists('setFlash')){
+if(!function_exists('flash_set')){
 /**
  * Set the flash message in session
  * This function is overwritable from the custom helpers/session_helper.php 
  *
- * @param $msg string The message to be shown
- * @param $name string The optional session name to store the message
- * @param $class string The HTML class name; default is success
+ * @param string $msg The message to be shown
+ * @param string $name The optional session name to store the message
+ * @param string $class The HTML class name; default is success
  *
  * @return void
+ */
+	function flash_set($msg, $name='', $class='success'){
+		setFlash($msg, $name, $class);
+	}
+}
+
+if(!function_exists('setFlash')){
+/**
+ * Alias of flash_set()
+ * Set the flash message in session
+ * This function is overwritable from the custom helpers/session_helper.php 
  */
 	function setFlash($msg, $name='', $class='success'){
 		$msg = '<span class="'.$class.'">'.$msg.'</span>';
@@ -98,15 +130,27 @@ if(!function_exists('setFlash')){
 		else $_SESSION[S_PREFIX.'flashMessage'] = $msg;
 	}
 }
-if(!function_exists('getFlash')){
+
+if(!function_exists('flash_get')){
 /**
  * Get the flash message from session and then delete it
  * This function is overwritable from the custom helpers/session_helper.php
  *
- * @param $name string The optional session name to retrieve the message from
+ * @param $name  string The optional session name to retrieve the message from
  * @param $class string The HTML class name; default is success
  *
  * @return string The HTML message
+ */
+	function flash_get($name='', $class='success'){
+		return getFlash($name, $class);
+	}
+}
+
+if(!function_exists('getFlash')){
+/**
+ * Alias of flash_get()
+ * Get the flash message from session and then delete it
+ * This function is overwritable from the custom helpers/session_helper.php
  */
 	function getFlash($name='', $class='success'){
 		$message = '';
