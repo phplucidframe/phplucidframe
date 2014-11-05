021effb4d3bc50ee544f8ff5261986a3c75d410c
Sithu <cithukyaw@gmail.com>
2014-11-05 10:46:30 +0630

    Bootstrap functions and system internal variables are organized.

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -32,123 +32,12 @@ define('FILE', ROOT.'files/');
 # path to files/cache filder
 define('CACHE', FILE.'cache/');
 
+# System prerequisites
+require_once INC . 'lc.inc';
 # System configuration variables
-require INC . 'config.php';
-
-$lc_sitewideWarnings = array();
-
-if( !isset($lc_languages) || (isset($lc_languages) && !is_array($lc_languages)) ){
-	$lc_languages = array('en' => 'English');
-}
-
-$requestURI = trim(ltrim($_SERVER['REQUEST_URI'], '/'.$lc_baseURL)); # /base-dir/path/to/sub/dir to path/to/sub/dir
-$requestURI = substr( $_SERVER['REQUEST_URI'], strpos($_SERVER['REQUEST_URI'], '/'.$lc_baseURL) + strlen($lc_baseURL) + 1 );
-$requestURI = ltrim($requestURI, '/');
-$request 	= explode('/', $requestURI);
-$lc_namespace = $request[0];
-
-# Clean lang code in URL
-if(array_key_exists($lc_namespace, $lc_languages)){
-	array_shift($request);
-	$requestURI = ltrim(ltrim($requestURI, $lc_namespace), '/'); # clean the language code from URI
-	if(count($request)) $lc_namespace = $request[0];
-	else $lc_namespace = '';
-}
-
-if( !(isset($lc_sites) && is_array($lc_sites) && array_key_exists($lc_namespace, $lc_sites)) ){
-	$lc_namespace = '';
-}
-
-# REQUEST_URI excluding the base URL
-define('REQUEST_URI', trim($requestURI, '/'));
-# Namespace according to the site directories
-define('LC_NAMESPACE', $lc_namespace);
-
-unset($requestURI);
-unset($request);
-
-/**
- * File include helper
- * Find files under the default directories inc/, js/, css/ according to the defined site directories $lc_sites
- *
- * @param $file	string File name with directory path
- * @param $recursive boolean True to find the file name until the site root
- *
- * @return string File name with absolute path if it is found, otherwise return an empty string
- */
-function _i($file, $recursive=true){
-	global $lc_baseURL;
-	global $lc_sites;
-	global $lc_languages;
-
-	$ext = strtolower(substr($file, strrpos($file, '.')+1)); # get the file extension
-	if( in_array($ext, array('js', 'css')) ){
-		$appRoot = WEB_APP_ROOT;
-		$root 	 = WEB_ROOT;
-		$clientFile = true;
-	}else{
-		$appRoot = APP_ROOT;
-		$root 	 = ROOT;
-		$clientFile = false;
-	}
-
-	if( !is_array($lc_languages) ){
-		$lc_languages = array('en' => 'English');
-	}
-
-	$requestURI = trim(ltrim($_SERVER['REQUEST_URI'], '/'.$lc_baseURL)); # /base-dir/path/to/sub/dir to path/to/sub/dir
-	$request 	= explode('/', $requestURI);
-
-	$needle = $request[0];
-	# Clean lang code in URL
-	if(array_key_exists($needle, $lc_languages)){
-		array_shift($request);
-		if(count($request)) $needle = $request[0];
-		else $needle = '';
-	}
-
-	if( LC_NAMESPACE == '' ){
-	# Find in APP_ROOT -> ROOT
-		$folders = array(
-			APP_ROOT 	=> $appRoot,
-			ROOT 		=> $root
-		);
-
-	}
-		
-	if(isset($lc_sites) && is_array($lc_sites) && count($lc_sites)){
-		if(array_key_exists(LC_NAMESPACE, $lc_sites)){
-		# Find in SUB-DIR -> APP_ROOT -> ROOT
-			$folders = array(
-				APP_ROOT.$lc_sites[LC_NAMESPACE].'/'	=> $appRoot . $lc_sites[LC_NAMESPACE] . '/',
-				APP_ROOT 		 			=> $appRoot,
-				ROOT 			 			=> $root
-			);
-		}
-	}
-	
-	# $key is for file_exists()
-	# $value is for include() or <script> or <link>
-	foreach($folders as $key => $value){
-		$fileWithPath = $key . $file;
-		if( is_file($fileWithPath) && file_exists($fileWithPath) ){
-			$fileWithPath = $value . $file;
-			return $fileWithPath;
-		}
-		if($recursive == false) break;
-	}	
-	
-	if(strstr($_SERVER['PHP_SELF'], APP_DIR)){
-		if(is_file($file) && file_exists($file)){
-			return $file;
-		}
-		if($recursive == true){
-			return $root . $file;
-		}		
-	}else{
-		return '';
-	}
-}
+require_once INC . 'config.php';
+# Load environment settings
+__envLoader();
 
 # DB configuration & DB helper (required)
 if(isset($lc_databases[$lc_defaultDbConnection]) && is_array($lc_databases[$lc_defaultDbConnection]) && $lc_databases[$lc_defaultDbConnection]['engine']){
