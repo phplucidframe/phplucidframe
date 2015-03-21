9877a841b557ec0df8f73f142811c7fc2940d802
Sithu <cithukyaw@gmail.com>
2015-03-21 16:52:25 +0630

    Updated social icons.

--- app/inc/tpl/footer.php
+++ app/inc/tpl/footer.php
@@ -14,6 +14,7 @@
 				<ul class="social-icons">
 					<li><a href="https://fb.com/lucidframe.myanmar" class="fb" target="_blank">Facebook</a></li>
 					<li><a href="https://twitter.com/phplucidframe" class="tw" target="_blank">Twitter</a></li>
+					<li><a href="http://goo.gl/PCfSvO" class="gplus" target="_blank">Google+</a></li>
 				</ul>
 			 </div>
 		</div>
