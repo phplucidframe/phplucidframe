e7d2d0378051361db3b1025ce9864ff0dc31c295
Sithu <cithukyaw@gmail.com>
2015-04-22 12:27:02 +0630

    Added more example menus.

--- app/inc/tpl/header.php
+++ app/inc/tpl/header.php
@@ -27,10 +27,19 @@
 						<a href="<?php echo _url('home'); ?>" <?php if(_arg(0) == 'home') echo 'class="active"'; ?>><?php echo _t('Welcome'); ?></a>
 					</li>
 					<li>
-						<a href="<?php echo _url('example/blog'); ?>" <?php if(in_array(_arg(1), array('blog', 'blog-page'))) echo 'class="active"'; ?>><?php echo _t('Example 1'); ?></a>
+						<a href="<?php echo _url('example/blog'); ?>" <?php if(in_array(_arg(1), array('blog'))) echo 'class="active"'; ?> title="AJAX List Example"><?php echo _t('Example 1'); ?></a>
 					</li>
 					<li>
-						<a href="<?php echo _url('example/articles'); ?>" <?php if(_arg(1) == 'articles') echo 'class="active"'; ?>><?php echo _t('Example 2'); ?></a>
+						<a href="<?php echo _url('example/articles'); ?>" <?php if(_arg(1) == 'articles') echo 'class="active"'; ?> title="Ordinary List Example"><?php echo _t('Example 2'); ?></a>
+					</li>
+					<li>
+						<a href="<?php echo _url('blog/2/url-rewrite-to-a-lucid-page-including-a-form-example'); ?>" <?php if(_arg(1) == 'blog-page') echo 'class="active"'; ?> title="AJAX Form Example"><?php echo _t('Example 3'); ?></a>
+					</li>
+					<li>
+						<a href="<?php echo _url('example/comment'); ?>" <?php if(_arg(1) == 'comment') echo 'class="active"'; ?> title="Generic Form Example"><?php echo _t('Example 4'); ?></a>
+					</li>
+					<li>
+						<a href="<?php echo _url('example/asyn-file-uploader'); ?>" <?php if(_arg(1) == 'asyn-file-uploader') echo 'class="active"'; ?> title="AsynFileUploader Example"><?php echo _t('Example 5'); ?></a>
 					</li>
 					<li>
 						<a href="http://phplucidframe.sithukyaw.com/downloads" target="_blank">Downloads</a>
