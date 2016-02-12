a330957329cf242f35b8348508b7231bf3eece4b
Sithu <cithukyaw@gmail.com>
2016-02-12 21:53:38 +0630

    Update menu

--- app/inc/tpl/header.php
+++ app/inc/tpl/header.php
@@ -24,7 +24,7 @@
                 </div>
                 <ul id="menu" class="clearfix">
                     <li>
-                        <a href="<?php echo _url('home'); ?>" <?php if (_arg(0) == 'home') echo 'class="active"'; ?>><?php echo _t('Welcome'); ?></a>
+                        <a href="<?php echo _url('home'); ?>" <?php if (_arg(0) == '' || _arg(0) == 'home') echo 'class="active"'; ?>><?php echo _t('Welcome'); ?></a>
                     </li>
                     <li>
                         <a href="<?php echo _url('example/blog'); ?>" <?php if (in_array(_arg(1), array('blog'))) echo 'class="active"'; ?> title="AJAX List Example"><?php echo _t('Example 1'); ?></a>
