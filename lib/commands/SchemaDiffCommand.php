3ee7aa4187be6a481a2eaa474df6ba3319a81a5f
Sithu <cithukyaw@gmail.com>
2017-03-30 21:47:18 +0630

    Implement `schema:diff` command

---
+++ lib/commands/SchemaDiffCommand.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This file is part of the PHPLucidFrame library.
+ * The script executes the command `php lucidframe schema:diff [<db>]`
+ *
+ * Usage:
+ *      schema:diff [<db>]
+ *
+ * Arguments:
+ *      db      The database namespace defined in $lc_databases of config.php [default: "default"]
+ *
+ * @package     PHPLucidFrame\Console
+ * @since       PHPLucidFrame v 1.17.0
+ * @copyright   Copyright (c), PHPLucidFrame.
+ * @author      Sithu K. <cithukyaw@gmail.com>
+ * @link        http://www.phplucidframe.com
+ * @license     http://www.opensource.org/licenses/mit-license.php MIT License
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE
+ */
+
+use LucidFrame\Core\SchemaManager;
+
+_consoleCommand('schema:diff')
+    ->setDescription('Generates the SQL (.sqlc) by comparing your current database to your mapping metadata.')
+    ->addArgument('db', 'The database namespace defined in $lc_databases of config.php', 'default')
+    ->setDefinition(function(\LucidFrame\Console\Command $cmd) {
+        $db = $cmd->getArgument('db');
+
+        $schema = _schema($db);
+        if ($schema === null) {
+            _writeln('Failed to load schema.');
+        } elseif ($schema === false) {
+            _writeln('Unable to find the schema file "%s".', DB.'schema.'.$db.'.php');
+        } else {
+            $sm = new SchemaManager($schema, $db);
+            if ($sm->diff($cmd, $db)) {
+                _writeln('Done.');
+            } else {
+                _writeln('No schema changed.');
+            }
+        }
+    })
+    ->register();
