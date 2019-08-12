9739c5444d91afa46a20176abfde2b6f87eb2ef4
Sithu <cithukyaw@gmail.com>
2019-08-12 13:24:47 +0630

    Change footer social links

--- app/inc/tpl/footer.php
+++ app/inc/tpl/footer.php
@@ -12,9 +12,8 @@
                     <span id="right"><?php echo _cfg('siteName'); ?></span>
                 </div>
                 <ul class="social-icons">
-                    <li><a href="https://fb.com/lucidframe.myanmar" class="fb" target="_blank">Facebook</a></li>
+                    <li><a href="http://bit.ly/fbphplucidframe" class="fb" target="_blank">Facebook</a></li>
                     <li><a href="https://twitter.com/phplucidframe" class="tw" target="_blank">Twitter</a></li>
-                    <li><a href="http://goo.gl/PCfSvO" class="gplus" target="_blank">Google+</a></li>
                 </ul>
              </div>
         </div>
