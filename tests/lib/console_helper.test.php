529e5c432f68d6979728f7fed3062829a483b475
Sithu <cithukyaw@gmail.com>
2019-12-15 23:39:55 +0630

    Fix console command test case

--- tests/lib/console_helper.test.php
+++ tests/lib/console_helper.test.php
@@ -8,77 +8,25 @@ use LucidFrame\Test\LucidFrameTestCase;
  */
 class ConsoleCommandTestCase extends LucidFrameTestCase
 {
-    public function testSecretGenerateCommand()
+    public function testCommands()
     {
-        $name = 'secret:generate';
+        $list = array(
+            'db:seed',
+            'env',
+            'list',
+            'schema:build',
+            'schema:diff',
+            'schema:export',
+            'schema:load',
+            'schema:update',
+            'secret:generate',
+        );
+
         $commands = Console::getCommands();
 
-        $this->assertTrue(isset($commands[$name]));
+        foreach ($list as $name) {
+            $this->assertTrue(isset($commands[$name]));
+        }
 
-        $cmd = $commands[$name];
-
-        list($args, $opts) = $cmd->parseArguments(array('--method', 'sha1', '--data', 'hello'));
-        $this->assertEqual($opts, array(
-            'help'   => null,
-            'method' => 'sha1',
-            'data'   => 'hello'
-        ));
-        $this->assertEqual($args, array());
-
-        list($args, $opts) = $cmd->parseArguments(array('--method=sha1', '--data=hello'));
-        $this->assertEqual($opts, array(
-            'help'   => null,
-            'method' => 'sha1',
-            'data'   => 'hello'
-        ));
-        $this->assertEqual($args, array());
-
-        list($args, $opts) = $cmd->parseArguments(array('-m', 'sha1', '--data=hello'));
-        $this->assertEqual($opts, array(
-            'help'   => null,
-            'method' => 'sha1',
-            'data'   => 'hello'
-        ));
-        $this->assertEqual($args, array());
-
-        list($args, $opts) = $cmd->parseArguments(array('-m', 'sha1', '-d', 'hello'));
-        $this->assertEqual($opts, array(
-            'help'   => null,
-            'method' => 'sha1',
-            'data'   => 'hello'
-        ));
-        $this->assertEqual($args, array());
-
-        list($args, $opts) = $cmd->parseArguments(array('-m=sha1', '-d=hello'));
-        $this->assertEqual($opts, array(
-            'help'   => null,
-            'method' => 'sha1',
-            'data'   => 'hello'
-        ));
-        $this->assertEqual($args, array());
-
-        list($args, $opts) = $cmd->parseArguments(array('-m=sha1', '--data=hello world'));
-        $this->assertEqual($opts, array(
-            'help'   => null,
-            'method' => 'sha1',
-            'data'   => 'hello world'
-        ));
-        $this->assertEqual($args, array());
-
-        list($args, $opts) = $cmd->parseArguments(array('--data=hello world'));
-        $this->assertEqual($opts, array(
-            'help'   => null,
-            'method' => 'md5',
-            'data'   => 'hello world'
-        ));
-        $this->assertEqual($args, array());
-
-        list($args, $opts) = $cmd->parseArguments(array('--method=sha1'));
-        $this->assertEqual($opts, array(
-            'help'   => null,
-            'method' => 'sha1',
-            'data'   => null
-        ));
-        $this->assertEqual($args, array());
     }
 }
