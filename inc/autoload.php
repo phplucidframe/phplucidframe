a96d56612a350faae676f4d74f7869f624a336ff
Sithu <cithukyaw@gmail.com>
2020-10-13 22:28:10 +0630

    Add more explanation comments in /inc/autoload.php

--- inc/autoload.php
+++ inc/autoload.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * This file is used to load vendor files and to unload library files globally
+ * It is useful when you don't use composer and install third-party packages in the vendor folder manually
+ * You may not use this configuration if you use composer
  */
 // For example,
 // _unloader('file_helper', HELPER);
