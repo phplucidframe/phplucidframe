37d4b15a3f091577b1d78eb5fb663827cfe93b64
Sithu <cithukyaw@gmail.com>
2021-01-16 23:22:49 +0630

    Add utility function to convert en number to mm number

--- lib/helpers/utility_helper.php
+++ lib/helpers/utility_helper.php
@@ -2338,3 +2338,26 @@ function _addHeadScript($file)
     $view = _app('view');
     $view->addHeadScript($file);
 }
+
+/**
+ * Convert English number to Myanmar number
+ * @param string $num
+ * @return string
+ */
+function _en2myNum($num)
+{
+    $digits = array(
+        '/0/' => '၀',
+        '/1/' => '၁',
+        '/2/' => '၂',
+        '/3/' => '၃',
+        '/4/' => '၄',
+        '/5/' => '၅',
+        '/6/' => '၆',
+        '/7/' => '၇',
+        '/8/' => '၈',
+        '/9/' => '၉',
+    );
+
+    return preg_replace(array_keys($digits), array_values($digits), $num);
+}
