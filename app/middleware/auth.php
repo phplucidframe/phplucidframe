d83f3cb19f91fc679e159eb2285fcedea4891b7f
Sithu <cithukyaw@gmail.com>
2021-07-14 22:55:54 +0630

    Add on() method to middleware for route registration

--- app/middleware/auth.php
+++ app/middleware/auth.php
@@ -1,10 +1,9 @@
 <?php
 
-if (route_start(_cfg('baseDir'), array(_cfg('baseDir') . '/login', _cfg('baseDir') . '/logout'))) {
-    _middleware(function () {
-        if (auth_isAnonymous()) {
-            flash_set('You are not authenticated. Please log in.', '', 'error');
-            _redirect(_cfg('baseDir') . '/login');
-        }
-    }, 'before');
-}
+_middleware(function () {
+    if (auth_isAnonymous()) {
+        flash_set('You are not authenticated. Please log in.', '', 'error');
+        _redirect(_cfg('baseDir') . '/login');
+    }
+})->on('startWith', _cfg('baseDir'))
+    ->on('except', array(_cfg('baseDir') . '/login', _cfg('baseDir') . '/logout'));
--- lib/classes/Middleware.php
+++ lib/classes/Middleware.php
@@ -24,10 +24,19 @@ class Middleware
     const BEFORE = 'before';
     const AFTER = 'after';
 
+    const FILTER_START_WITH = 'startWith';
+    const FILTER_CONTAIN    = 'contain';
+    const FILTER_EQUAL      = 'equal';
+    const FILTER_EXCEPT     = 'except';
+
     /** @var array Array of registered middlewares (before) */
     private static $before = array();
     /** @var array Array of registered middlewares (after) */
     private static $after = array();
+    /** @var string Unique id */
+    private static $id;
+    /** @var array */
+    private static $routeFilters = array();
 
     /**
      * Register a middleware
@@ -37,30 +46,90 @@ class Middleware
      */
     public function register(\Closure $closure, $event = self::BEFORE)
     {
+        self::$id = uniqid();
+
         if (in_array($event, array(self::BEFORE, self::AFTER))) {
-            self::${$event}[] = $closure;
+            self::${$event}[self::$id] = $closure;
+        }
+
+        return $this;
+    }
+
+    public function on($key, $value)
+    {
+        if (self::$id) {
+            self::$routeFilters[self::$id][$key][] = $value;
         }
 
         return $this;
     }
 
     /**
-     * Run all registered middleware (before)
+     * Run all registered middlewares (before)
      */
     public static function runBefore()
     {
-        foreach (self::$before as $closure) {
-            $closure();
-        }
+        self::invoke(self::$before);
     }
 
     /**
-     * Run all registered middleware (after)
+     * Run all registered middlewares (after)
      */
     public static function runAfter()
     {
-        foreach (self::$after as $closure) {
-            $closure();
+        self::invoke(self::$after);
+    }
+
+    /**
+     * Run the registered middlewares
+     * @param array $middlewares List of middlewares
+     */
+    private static function invoke(array $middlewares)
+    {
+        foreach ($middlewares as $id => $closure) {
+            if (isset(self::$routeFilters[$id])) {
+                $except = array();
+                if (isset(self::$routeFilters[$id][self::FILTER_EXCEPT])) {
+                    foreach (self::$routeFilters[$id][self::FILTER_EXCEPT] as $exp) {
+                        $exp = is_array($exp) ? $exp : array($exp);
+                        $except = array_merge($except, $exp);
+                    }
+                    unset(self::$routeFilters[$id][self::FILTER_EXCEPT]);
+                }
+
+                if (count(self::$routeFilters[$id])) {
+                    foreach (self::$routeFilters[$id] as $filter => $value) {
+                        foreach ($value as $val) {
+                            switch($filter) {
+                                case self::FILTER_START_WITH:
+                                    if (route_start($val, $except)) {
+                                        $closure();
+                                    }
+                                    break;
+
+                                case self::FILTER_CONTAIN:
+                                    $val = is_array($val) ? $val : array($val);
+                                    if (call_user_func_array('route_contain', $val)) {
+                                        $closure();
+                                    }
+                                    break;
+
+                                case self::FILTER_EQUAL:
+                                    if (route_equal($val)) {
+                                        $closure();
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                } else {
+                    if (count($except) && call_user_func_array('route_except', $except)) {
+                        $closure();
+                    }
+                }
+            } else {
+                $closure();
+            }
         }
     }
 }
