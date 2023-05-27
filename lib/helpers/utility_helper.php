414973f8489ba58d1ccf7ad370504658be64d670
Sithu <cithukyaw@gmail.com>
2023-05-27 20:23:57 +0630

    Fix randomCode helper to include capital letters

--- lib/helpers/utility_helper.php
+++ lib/helpers/utility_helper.php
@@ -1590,7 +1590,7 @@ if (!function_exists('_randomCode')) {
     {
         # Letters & Numbers for default
         if (sizeof($letters) == 0) {
-            $letters = array_merge(range(0, 9), range('a', 'z'));
+            $letters = array_merge(range(0, 9), range('a', 'z'), range('A', 'Z'));
         }
 
         shuffle($letters); # Shuffle letters
